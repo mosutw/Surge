1a0e98610665ad8257052ae7d8adab1ff01d5e4a
mosutw <luke1974@gmail.com>
2023-01-09 09:08:19 +0800

    commit

--- Scripts/fetnet_checkin_info.js
+++ Scripts/fetnet_checkin_info.js
@@ -3,44 +3,7 @@ function fetnetNotify(subtitle = '', message = '') {
 };
 
 //2
-// if ($request.method === 'POST') {
-//   // fetnetNotify('test1');
-//   const cookie = $request.headers['Cookie'] || $request.headers['cookie'];
-//   fetnetNotify($request.url);
-//   console.log('url:' + $request.url)
-//   // if (cookie && $request.body) {
-//   //   try {
-//   //     let body = JSON.parse($request.body);
-//   //     if (body.doAction === 'list') {
-//   //       body.pNo = '';
-//   //       body.doAction = 'reg';
-//   //       const saveCookie = $persistentStore.write(cookie, 'fetnetCookie');
-//   //       const saveBody = $persistentStore.write(JSON.stringify(body), 'fetnetBody');
-//   //       if (!(saveCookie && saveBody)) {
-//   //         fetnetNotify(
-//   //           '保存失敗 ‼️',
-//   //           '請稍後嘗試'
-//   //         );
-//   //       } else {
-//   //         fetnetNotify(
-//   //           '保存成功 🍪',
-//   //           ''
-//   //         );
-//   //       }
-//   //     }
-//   //   } catch (error) {
-//   //     fetnetNotify(
-//   //       '保存失敗 ‼️',
-//   //       error
-//   //     );
-//   //   }
-//   // } else {
-//   //   fetnetNotify(
-//   //     '保存失敗 ‼️',
-//   //     '請重新登入'
-//   //   );
-//   // }
-// }
+
 if ($request.method === 'GET') {
   // fetnetNotify('test1');
   const cookie = $request.headers['Cookie'] || $request.headers['cookie'];
@@ -79,4 +42,43 @@ if ($request.method === 'GET') {
   //   );
   // }
 }
+
+// if ($request.method === 'POST') {
+//   // fetnetNotify('test1');
+//   const cookie = $request.headers['Cookie'] || $request.headers['cookie'];
+//   fetnetNotify($request.url);
+//   console.log('url:' + $request.url)
+//   // if (cookie && $request.body) {
+//   //   try {
+//   //     let body = JSON.parse($request.body);
+//   //     if (body.doAction === 'list') {
+//   //       body.pNo = '';
+//   //       body.doAction = 'reg';
+//   //       const saveCookie = $persistentStore.write(cookie, 'fetnetCookie');
+//   //       const saveBody = $persistentStore.write(JSON.stringify(body), 'fetnetBody');
+//   //       if (!(saveCookie && saveBody)) {
+//   //         fetnetNotify(
+//   //           '保存失敗 ‼️',
+//   //           '請稍後嘗試'
+//   //         );
+//   //       } else {
+//   //         fetnetNotify(
+//   //           '保存成功 🍪',
+//   //           ''
+//   //         );
+//   //       }
+//   //     }
+//   //   } catch (error) {
+//   //     fetnetNotify(
+//   //       '保存失敗 ‼️',
+//   //       error
+//   //     );
+//   //   }
+//   // } else {
+//   //   fetnetNotify(
+//   //     '保存失敗 ‼️',
+//   //     '請重新登入'
+//   //   );
+//   // }
+// }
 $done({})
