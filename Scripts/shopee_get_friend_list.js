31be4b60c2807091279794cc36fae495d0d49a89
mosutw <luke1974@gmail.com>
2023-01-28 12:31:54 +0800

    add shopee_get_friend_list 取得朋友列表

---
+++ Scripts/shopee_get_friend_list.js
@@ -0,0 +1,67 @@
+const shopeeCookie = $persistentStore.read('CookieSP') + ';SPC_EC=' + $persistentStore.read('SPC_EC') + ';';
+const shopeeCSRFToken = $persistentStore.read('CSRFTokenSP');
+const shopeeHeaders = {
+  'Cookie': shopeeCookie,
+  'X-CSRFToken': shopeeCSRFToken,
+};
+function shopeeNotify(subtitle = '', message = '') {
+  $notification.post('🍤 蝦皮免運寶箱', subtitle, message, { 'url': 'shopeetw://' });
+};
+
+let shopeeGetFriendIdRequest = {
+  url: 'https://games.shopee.tw/farm/api/message/get?page=1&pageSize=100',
+  headers: shopeeHeaders,
+};
+
+
+// 取得朋友列表
+function ShopeeGetFriendId() {
+  $httpClient.get(shopeeGetFriendIdRequest, function (error, response, data) {
+    if (error) {
+      shopeeNotify(
+        '朋友列表取得失敗 ‼️',
+        '連線錯誤'
+      );
+      $done();
+    } else {
+      if (response.status === 200) {
+        try {
+          const obj = JSON.parse(data);
+          console.log(data);
+          if (obj.msg === 'success') {
+            const eventUrl = obj.data.basic.event_code;
+            let module_id = 0;
+            let found = false;
+            for (const item of obj.data.modules) {
+              if (item.module_name === 'Service.LUCKY_DRAW_COMPONENT') {
+                module_id = item.module_id;
+                found = true;
+                break;
+              }
+            }
+          } else {
+            shopeeNotify(
+              '朋友列表取得失敗 ‼️',
+              obj.msg
+            );
+            $done();
+          }
+        } catch (error) {
+          shopeeNotify(
+            '朋友列表取得失敗 ‼️',
+            error
+          );
+          $done();
+        }
+      } else {
+        shopeeNotify(
+          'Cookie 已過期 ‼️',
+          '請重新登入'
+        );
+        $done();
+      }
+    }
+  });
+}
+
+eventListGetActivity();
