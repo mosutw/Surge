e10d18a2af1addeb6ef1ad62ef1a04c8626414be
mosutw <luke1974@gmail.com>
2023-01-28 22:41:06 +0800

    fix shopee_help_friend_water

--- Scripts/shopee_help_friend_water.js
+++ Scripts/shopee_help_friend_water.js
@@ -30,7 +30,7 @@ let shopeeHelpFriendWaterRequest = {
 
 
 // 幫朋友澆水
-function shopeehHelpFriendWater() {
+function shopeeHelpFriendWater() {
   for (const Friend of shopeeFriendsInfo) {
     try {
       // shopeeHelpFriendWaterRequest.body.friendId = Friend.FriendId;
