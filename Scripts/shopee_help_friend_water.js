297f3cfcc7477be2d677296aa071cd3e3655dab6
mosutw <luke1974@gmail.com>
2023-01-30 21:00:48 +0800

    fix help friend water

--- Scripts/shopee_help_friend_water.js
+++ Scripts/shopee_help_friend_water.js
@@ -47,7 +47,7 @@ async function shopeeGetFriendCrop(Friend) {
         headers: shopeeHeaders
       };
       // console.log(request);
-      $httpClient.get(request, function (error, response, data) {
+      $httpClient.get(request, async function (error, response, data) {
         if (error) {
           console.log('取得朋友CronId失敗1 ‼️', '請重新登入');
           $done();
@@ -62,7 +62,7 @@ async function shopeeGetFriendCrop(Friend) {
               console.log(shopeeHelpFriendWaterRequest.body);
               console.log('-----------------');
               // $done();
-              // await shopeeHelpFriendWater(shopeeHelpFriendWaterRequest);
+              await shopeeHelpFriendWater(shopeeHelpFriendWaterRequest);
               // // $done();
 
             } else {
@@ -106,12 +106,12 @@ async function shopeeHelpFriendWater(shopeeHelpFriendWaterRequest) {
           if (obj1.msg === 'success') {
             // console.log('ok------------');                      
             // console.log(obj);
-            CropOK += 1;
-            console.log('幫朋友澆水失敗1');        
+            // CropOK += 1;
+            console.log('幫朋友澆水成功');        
             // $done();          
           }
           else {
-            CropFail += 1;
+            // CropFail += 1;
             console.log('幫朋友澆水失敗1');        
           }
         }
