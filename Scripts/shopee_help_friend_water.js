b6b66c3bd8462f086a70b9bab16d5f8123b0fc0b
mosutw <luke1974@gmail.com>
2023-01-30 22:59:48 +0800

    fix help friend water

--- Scripts/shopee_help_friend_water.js
+++ Scripts/shopee_help_friend_water.js
@@ -48,8 +48,9 @@ async function shopeeGetFriendCrop(Friend) {
         url: `https://games.shopee.tw/farm/api/friend/orchard/context/get?friendId=` + Friend.FriendId,
         headers: shopeeHeaders
       };
-      console.log(request);
+      // console.log(request);
       $httpClient.get(request, async function (error, response, data) {
+        console.log(response);
         if (error) {
           console.log('取得朋友CronId失敗1 ‼️', '請重新登入');
           // $done();
