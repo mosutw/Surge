ae177cfe68c7bede46652d0e59b7ab780afcd9ac
mosutw <luke1974@gmail.com>
2023-01-31 09:58:18 +0800

    fix shopee_help_friend_water

--- Scripts/shopee_help_friend_water.js
+++ Scripts/shopee_help_friend_water.js
@@ -171,9 +171,9 @@ async function loopGetRequest() {
   let CropFail = 0;
   Friend = JSON.parse(shopeeFriendsInfo)[0];
   // for (const Friend of JSON.parse(shopeeFriendsInfo)) {
-    console.log('1------');
+    // console.log('1------');
     let RequestData = await shopeeGetFriendCrop(Friend);
-    console.log('a----------');
+    // console.log('a----------');
     console.log(RequestData);
     // if (RequestData !== 'error') {
     //   let result = await shopeeHelpFriendWater(RequestData);
@@ -184,32 +184,33 @@ async function loopGetRequest() {
   // $done();
 }
 // shopeeGetFriendCrop();
-// loopGetRequest();
+loopGetRequest();
 
-async function A(num) {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      // num += 1;
-      resolve(num + 1);
-    }, 100);
-  });
-}
+// -----------
+// async function A(num) {
+//   return new Promise(resolve => {
+//     setTimeout(() => {
+//       // num += 1;
+//       resolve(num + 1);
+//     }, 100);
+//   });
+// }
 
-async function B(num) {
-  return num + 1;
-}
+// async function B(num) {
+//   return num + 1;
+// }
 
-async function loop() {
-  let num = 0;
-  for (let i = 0; i < 10; i++) {
-    num = await A(num);
-    num = await B(num);
-    console.log(num);
-  }
-  $done();
-}
+// async function loop() {
+//   let num = 0;
+//   for (let i = 0; i < 10; i++) {
+//     num = await A(num);
+//     num = await B(num);
+//     console.log(num);
+//   }
+//   $done();
+// }
 
-loop();
+// loop();
 
 
 
