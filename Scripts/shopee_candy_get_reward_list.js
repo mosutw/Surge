a670fc2bbb45680027534e413470fe1e485c1622
mosutw <luke1974@gmail.com>
2023-02-01 20:22:27 +0800

    fix 蝦皮消消樂獎勵兌換

--- Scripts/shopee_candy_get_reward_list.js
+++ Scripts/shopee_candy_get_reward_list.js
@@ -1,4 +1,4 @@
-//20230201-16
+//20230201-17
 // const shopeeCookie = $persistentStore.read('CookieSP') + ';SPC_EC=' + $persistentStore.read('SPC_EC') + ';';
 // const shopeeCSRFToken = $persistentStore.read('CSRFTokenSP');
 // const shopeeHeaders = {
@@ -213,7 +213,7 @@ async function redeemReward() {
     await shopeeCandyGetRewardList();
     // await shopeeCandyGetRewardList
     // .then
-    // await redeemReward();
+    await redeemReward();
     console.log(`✅ 蝦皮消消樂獎勵兌換列表成功: ${JSON.stringify(RewardList[0])}` );
     console.log(redeemRewardRequest);
 
