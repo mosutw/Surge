d85bea09799574f30344a8cc87150d2c6aab6ef8
mosutw <luke1974@gmail.com>
2023-02-02 15:39:33 +0800

    add shopee_pets_get_reward_list

--- Scripts/shopee_pets_get_reward_list.js
+++ Scripts/shopee_pets_get_reward_list.js
@@ -1,4 +1,4 @@
-//20230202-7
+//20230202-8
 let showNotification = true;
 let config = null;
 let RewardList = null;
@@ -116,7 +116,7 @@ async function shopeePetsGetRewardList() {
             if (obj.msg === 'success') {
               // RewardList = obj.data.item_list.filter(item => item.name.includes("蝦幣"))
               // RewardList = obj.data.item_list.filter(item => item.name.includes("蝦幣") || item.name.includes("免運寶箱"));
-              RewardList = obj.data.item_list.filter(item => item.name.includes("1.5 蝦幣"));
+              RewardList = obj.data.item_list.filter(item => item.name.includes("1.5 蝦幣"))
                 .sort((a, b) => parseFloat(b.name.split(' ')[0]) - parseFloat(a.name.split(' ')[0]));
               console.log('可兌換項目數:' + RewardList.length);
               // $done();
