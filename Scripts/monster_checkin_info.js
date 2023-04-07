6bbe4e527f7f5bae63a5ee02c19f7bf25788f451
mosutw <luke1974@gmail.com>
2023-04-07 16:57:36 +0800

    調整發票怪獸

--- Scripts/monster_checkin_info.js
+++ Scripts/monster_checkin_info.js
@@ -3,17 +3,17 @@ function surgeNotify(subtitle = '', message = '') {
 };
 
 if ($request.method === 'POST') {
-  const cookie = $request.headers['Cookie'] || $request.headers['cookie'];
   console.log($request);
-  if (cookie && $request.body) {
+  // const cookie = $request.headers['Cookie'] || $request.headers['cookie'];
+  if ($request.body) {
     try {
       let body = JSON.parse($request.body);
       if (body.doAction === 'list') {
         body.pNo = '';
         body.doAction = 'reg';
-        const saveCookie = $persistentStore.write(cookie, 'monsterCookie');
+        // const saveCookie = $persistentStore.write(cookie, 'monsterCookie');
         const saveBody = $persistentStore.write(JSON.stringify(body), 'monsterBody');
-        if (!(saveCookie && saveBody)) {
+        if (!(saveBody)) {
           surgeNotify(
             '保存失敗 ‼️',
             '請稍後嘗試'
