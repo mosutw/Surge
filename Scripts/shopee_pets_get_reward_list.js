8725898e3738043aa79b974c61b6542597b81e65
mosutw <luke1974@gmail.com>
2023-06-03 21:37:50 +0800

    修改寵物村兌換獎勵

--- Scripts/shopee_pets_get_reward_list.js
+++ Scripts/shopee_pets_get_reward_list.js
@@ -121,7 +121,7 @@ async function shopeePetsGetRewardList() {
             if (obj.msg === 'success') {
               // RewardList = obj.data.item_list.filter(item => item.name.includes("蝦幣"))
               // RewardList = obj.data.item_list.filter(item => item.name.includes("蝦幣") || item.name.includes("免運寶箱"));
-              RewardList = obj.data.item_list.filter(item => item.name.includes("3 蝦幣","1.5 蝦幣")).sort((a, b) => parseFloat(b.name.split(' ')[0]) - parseFloat(a.name.split(' ')[0]));
+              RewardList = obj.data.item_list.filter(item => item.name.includes("3 蝦幣") || item.name.includes("1.5 蝦幣") ).sort((a, b) => parseFloat(b.name.split(' ')[0]) - parseFloat(a.name.split(' ')[0]));
               // RewardList = obj.data.item_list.filter(item => item.name.includes("3 蝦幣")).sort((a, b) => parseFloat(b.name.split(' ')[0]) - parseFloat(a.name.split(' ')[0]));
               console.log('可兌換項目數:' + RewardList.length);
               // $done();
